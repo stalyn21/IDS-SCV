5ec1f3f6ca3de5de2ce97d0bdad910d84396400c
Stalyn Chancay <stalyn151996@gmail.com>
2022-08-18 18:18:33 -0500

    some commmet updated

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
+# install snort
 sudo apt install -y snort
 snort --version
+echo "==== Successfully Snort Installation ===="
 
+# Reemplace rules for snort
+echo "==== Reemplacing Local Rules ===="
 sudo mv /etc/snort/rules/local.rules /etc/snort/rules/local.rules.bak
 sudo cp IDS-SCV/snort_setup/rules/local.rules /etc/snort/rules/
-
+echo "==== Succesfully Snort Local Rules ===="
 
