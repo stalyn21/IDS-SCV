86eb200f76af2b65ea5c754fe66f6ce92f31a6c9
Stalyn Chancay <stalyn151996@gmail.com>
2022-08-18 15:00:26 -0500

    Adding snort rules

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
 sudo apt install -y snort
+snort --version
+
+sudo mv /etc/snort/rules/local.rules /etc/snort/rules/local.rules.bak
+sudo cp IDS-SCV/snort_setup/rules/local.rules /etc/snort/rules/
+
+
